e189fa32eb6509d4c5118aa45fc63e8e2bfa1f9a
joaquin-garcia-plante <joaquingarciaplante@gmail.com>
2022-07-13 09:52:50 -0300

    update

---
+++ Demo.js
@@ -0,0 +1,11 @@
+let num1 = Math.floor(Math.random() * 10) + 1;
+let num2 = Math.floor(Math.random() * 10) + 1;
+let num3 = Math.floor(Math.random() * 10) + 1;
+let arr = undefined;
+let str = "hola";
+console.log(typeof str);
+if (!arr) {
+  console.log("AAAAAAAAAAAAAAAAAAAA");
+}
+console.log(typeof arr);
+console.log(num1, num2, num3);
